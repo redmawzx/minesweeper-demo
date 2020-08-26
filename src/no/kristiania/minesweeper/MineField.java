a4fb587f11969680e0c9899b55f2ebfa3fddeafe
Jan-Andreas Rusnak <j4n4ndre4sr@gmail.com>
2020-08-26 17:32:39 +0200

    itShowsHintsAroundMine green, itCountsMinesAroundCell red

--- src/no/kristiania/minesweeper/MineField.java
+++ src/no/kristiania/minesweeper/MineField.java
@@ -19,7 +19,7 @@ public class MineField {
                     for (int r=row-1; r<=row+1; r++) {
                         for (int c=col-1; c<=col+1; c++) {
                             if (hasMine(r, c)) {
-                                hint = 1;
+                                hint += 1;
                             }
                         }
                     }
